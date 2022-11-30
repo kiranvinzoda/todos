034363f12247fc25d9bcd50b7ec2dc4ed8ab4597
kiranvinzoda <kiran.vinzoda@aeonx.digital>
2022-11-30 17:47:15 +0530

    add and update opration added

--- crud.py
+++ crud.py
@@ -4,16 +4,16 @@ import models
 import schemas
 
 
-def get_user(db: Session, user_id: int):
-    return db.query(models.User).filter(models.User.id == user_id).first()
+def get_todo(db: Session, todo_id: int):
+    return db.query(models.Todo).filter(models.Todo.id == todo_id).first()
 
 
 def get_todo_by_id(db: Session, id: int):
     return db.query(models.Todo).filter(models.Todo.id == id).first()
 
 
-def get_users(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.User).offset(skip).limit(limit).all()
+def get_all_todos(db: Session):
+    return db.query(models.Todo).all()
 
 
 def create_todo(db: Session, todo: schemas.Crate_Todo):
@@ -24,5 +24,18 @@ def create_todo(db: Session, todo: schemas.Crate_Todo):
     db.refresh(db_todo)
     return db_todo
 
+def update_todo(db: Session, todo_id: int, todo: schemas.Crate_Todo):
+    db_todo = db.query(models.Todo).filter(models.Todo.id == id).update(vars(todo))
+    db.commit()
+    return db_todo
 
+
+
+def delete_todo(db: Session, todo_id: int):
+    
+    db_todo = db.query(models.Todo).filter(models.Todo.id == id).first()
+    print(db_todo)
+    db_todo.is_active = False
+    db.commit()
+    return db_todo
 
--- main.py
+++ main.py
@@ -35,11 +35,50 @@ def get_db():
     finally:
         db.close()
 
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
-
-@app.post("/todo/", response_model=schemas.Show_Todo)
+@app.post("/add_todo/", response_model=schemas.Show_Todo)
 def create_todo(todo: schemas.Crate_Todo, db: Session = Depends(get_db)):
     return crud.create_todo(db=db, todo=todo)
+
+
+
+@app.get("/show_todos/", response_model=List[schemas.Show_Todo])
+def read_users(db: Session = Depends(get_db)):
+    todos = crud.get_all_todos(db)
+    return todos
+
+
+@app.get("/get_todo/{todo_id}", response_model=schemas.Show_Todo)
+def read_todo(todo_id: int, db: Session = Depends(get_db)):
+    db_todo = crud.get_todo(db, todo_id=todo_id)
+    if db_todo is None:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    return db_todo
+
+
+@app.put("/update_todo/{todo_id}", response_model=schemas.Show_Todo)
+def put_todo(todo_id: int, todo: schemas.Crate_Todo, db: Session = Depends(get_db)):
+    db_todo = crud.get_todo(db, todo_id=todo_id)
+
+    if db_todo is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    db_todo = crud.update_todo(db, todo_id=todo_id, todo = todo)    
+    return db_todo
+
+
+@app.delete("/delete_todo/{todo_id}")
+def read_todo(todo_id: int, db: Session = Depends(get_db)):
+    db_todo = crud.get_todo(db, todo_id=todo_id)
+    print(db_todo)
+    if db_todo is None:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    db_todo = crud.delete_todo(db, todo_id=todo_id)
+    if db_todo is None:
+        raise HTTPException(status_code=404, detail="User not found")
+   
 
 
 # @app.get("/users/", response_model=List[schemas.User])
