d7e731bab7eb6c8f187e326c6f713ea11b86efd0
kiranvinzoda <kiran.vinzoda@aeonx.digital>
2023-01-21 11:06:36 +0530

    again changes in databases onfig

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql://root:root@localhost:3306/todos"
+SQLALCHEMY_DATABASE_URL = "mysql://root:root@localhost:3306/todo"
 # SQLALCHEMY_DATABASE_URL = "mysql://kiran:admin@localhost:3308/fastapi"
 
 engine = create_engine(
